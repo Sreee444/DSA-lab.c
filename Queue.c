23e29eb73c3406b3789a83839c123ae214cd8c7d
Sreenandhu Jacy <wreakpanther100@gmail.com>
2024-09-01 10:18:06 +0530

    Add files via upload

---
+++ Queue.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+int A[100],front=-1,rear=-1,Max,choice;
+void Enqueue();
+void Dequeue();
+void Display();
+   void main()
+    {
+        printf("Enter the size of the Queue : ");
+        scanf("%d",&Max);
+        while(choice !=4)
+         {
+            printf("Choose an operation :\n");
+            printf("1 : Enqueue\n");
+            printf("2 : Dequeue\n");
+            printf("3 : Display\n");
+            printf("4 : Exit\n");
+            scanf("%d",&choice); 
+
+            switch (choice)
+            {
+            case 1:
+               Enqueue();
+               break;
+            case 2:
+               Dequeue();
+               break;
+            case 3:
+               Display();
+               break;
+            case 4:
+               printf("EXITED\n");
+               break;
+            default:
+               printf("Enter a valid option\n");
+                break;
+            }
+            
+         }
+    }
+
+  void Enqueue()
+   {
+    int item;
+    if(rear==Max-1)
+     {
+        printf("QUEUE IS FULL\n");
+     }
+    else 
+     {
+       if(front ==-1)
+        {
+         front=0;
+        }
+        rear++;
+        printf("Enter the number to be added : ");
+        scanf("%d",&item);
+        A[rear]=item;
+     }
+   }
+
+  void Dequeue()
+   {
+     int value;
+     if(front==rear+1)
+      {
+        printf("QUEUE IS EMPTY\n");
+      }
+     else 
+      {
+        value =A[front];
+        printf("The deleted element is %d\n", value);
+        front++;
+      }
+   }
+
+  void Display()
+   {
+    int i;
+    if(front == rear+1)
+     {
+        printf("QUEUE IS EMPTY\n");
+     }
+    else
+     {
+        for(i=front;i<=rear;i++)
+         {
+            printf("%d ",A[i]);
+         }
+         printf("\n");
+     }
+   }  
